190762c592ad0df406e962b458808687dfd04e08
KUMIN\kyung <insetour@naver.com>
2026-04-30 22:49:26 +0900

    내일 모레 할 일

--- 01_Server/Src/03_ServerApp/Source/main.cpp
+++ 01_Server/Src/03_ServerApp/Source/main.cpp
@@ -1,5 +1,6 @@
 #include <Boost/BoostNetEngineServer.h>
 
+// TODO : 시그널 처리 고민해보기 (부스트에도 <signal.h> 같은 시그널 관련 라이브러리가 있대. ex : boost::asio::signal_set)
 int main()
 {
     BoostNetEngineServer netEngine(12345, 1024, 5);
