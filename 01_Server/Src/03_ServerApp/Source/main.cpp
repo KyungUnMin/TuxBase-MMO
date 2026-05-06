bbe355206906bf8b088826b43b71d8f2a5106c84
KUMIN\kyung <insetour@naver.com>
2026-05-06 21:24:11 +0900

    프로토버퍼 적용

--- 01_Server/Src/03_ServerApp/Source/main.cpp
+++ 01_Server/Src/03_ServerApp/Source/main.cpp
@@ -1,13 +1,22 @@
 #include <Verify/SignalHandler.h>
 #include <Boost/BoostNetEngineServer.h>
+#include "Example.pb.h"
 
 int main()
 {
     SignalHandler::InitSignal();
-    BoostNetEngineServer netEngine(13000, 1024, 5);
-    netEngine.Start();
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    netEngine.Stop();
+
+    Protocol::S2C_LoginResponse loginResponse;
+    loginResponse.set_success(true);
+    loginResponse.set_message("Welcome to TuxBase MMO!");
+
+    // std::string serializedData;
+    // loginResponse.SerializeToString(&serializedData);
+
+    // BoostNetEngineServer netEngine(13000, 1024, 5);
+    // netEngine.Start();
+    // std::this_thread::sleep_for(std::chrono::seconds(10));
+    // netEngine.Stop();
 
     return 0;
 }
